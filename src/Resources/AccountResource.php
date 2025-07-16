8bba2572823b54f6fb4d9a6ab1a818d3f4c57458
Leanderson Paulo <leopaulo88@gmail.com>
2025-07-16 18:49:46 -0300

    Remove unnecessary docblocks from AccountResource methods

--- src/Resources/AccountResource.php
+++ src/Resources/AccountResource.php
@@ -6,19 +6,11 @@ use Leopaulo88\Asaas\Entities\Account\AccountCreate;
 
 class AccountResource extends BaseResource
 {
-    /**
-     * List subaccounts with optional filters
-     * Returns List entity automatically
-     */
     public function list(array $params = [])
     {
         return $this->get('/accounts', $params);
     }
 
-    /**
-     * Create a new subaccount
-     * Returns AccountResponse entity automatically
-     */
     public function create(array|AccountCreate $data)
     {
         if (is_array($data)) {
@@ -28,19 +20,11 @@ class AccountResource extends BaseResource
         return $this->post('/accounts', $data->toArray());
     }
 
-    /**
-     * Get subaccount by ID
-     * Returns AccountResponse entity automatically
-     */
     public function find(string $id)
     {
         return $this->get("/accounts/{$id}");
     }
 
-    /**
-     * Get the current account information
-     * Returns AccountResponse entity automatically
-     */
     public function me()
     {
         return $this->get('/myAccount');
