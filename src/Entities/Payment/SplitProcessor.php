6a9281c7e6596402c451f6374460b3ee3783144d
Tiago Oliveira <tiagoliveira555@gmail.com>
2025-08-04 18:52:58 -0300

    Refactor split item cleaning logic for installments

--- src/Entities/Payment/SplitProcessor.php
+++ src/Entities/Payment/SplitProcessor.php
@@ -26,53 +26,15 @@ class SplitProcessor extends BaseEntity
 
     private function cleanSplitArray(array &$splitItem, bool $isInstallment): void
     {
-        $hasPercentual = !empty($splitItem['percentualValue']);
-        $hasFixed = !empty($splitItem['fixedValue']);
-        $hasTotalFixed = !empty($splitItem['totalFixedValue']);
-
-        if ($hasPercentual) {
-            $this->removeFields($splitItem, ['fixedValue', 'totalFixedValue']);
-        } elseif ($isInstallment && $hasTotalFixed) {
-            $this->removeFields($splitItem, ['fixedValue', 'percentualValue']);
-        } elseif (!$isInstallment && $hasFixed) {
-            $this->removeFields($splitItem, ['percentualValue', 'totalFixedValue']);
-        } else {
-            $fieldsToRemove = $isInstallment ? ['fixedValue'] : ['totalFixedValue'];
-            $this->removeFields($splitItem, $fieldsToRemove);
-        }
+        $fieldToRemove = $isInstallment ? 'fixedValue' : 'totalFixedValue';
+        unset($splitItem[$fieldToRemove]);
     }
 
     private function cleanSplitObject(object $splitItem, bool $isInstallment): void
     {
-        $hasPercentual = property_exists($splitItem, 'percentualValue') && !empty($splitItem->percentualValue);
-        $hasFixed = property_exists($splitItem, 'fixedValue') && !empty($splitItem->fixedValue);
-        $hasTotalFixed = property_exists($splitItem, 'totalFixedValue') && !empty($splitItem->totalFixedValue);
-
-        if ($hasPercentual) {
-            $this->removeProperties($splitItem, ['fixedValue', 'totalFixedValue']);
-        } elseif ($isInstallment && $hasTotalFixed) {
-            $this->removeProperties($splitItem, ['fixedValue', 'percentualValue']);
-        } elseif (!$isInstallment && $hasFixed) {
-            $this->removeProperties($splitItem, ['percentualValue', 'totalFixedValue']);
-        } else {
-            $fieldsToRemove = $isInstallment ? ['fixedValue'] : ['totalFixedValue'];
-            $this->removeProperties($splitItem, $fieldsToRemove);
-        }
-    }
-
-    private function removeFields(array &$splitItem, array $fields): void
-    {
-        foreach ($fields as $field) {
-            unset($splitItem[$field]);
-        }
-    }
-
-    private function removeProperties(object $splitItem, array $properties): void
-    {
-        foreach ($properties as $property) {
-            if (property_exists($splitItem, $property)) {
-                unset($splitItem->$property);
-            }
+        $fieldToRemove = $isInstallment ? 'fixedValue' : 'totalFixedValue';
+        if (property_exists($splitItem, $fieldToRemove)) {
+            unset($splitItem->$fieldToRemove);
         }
     }
 }
