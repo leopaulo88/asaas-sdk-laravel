cd761ec9ef0ca3f4386b1d17daf842ea768904d2
Leanderson Paulo <52747724+leopaulo88@users.noreply.github.com>
2025-08-17 11:07:52 -0300

    Fix/account response (#12)
    * Add AccountNumber and CommercialInfoExpiration entities to account response
    
    * Fix walletId type in AccountResourceTest to string
    
    * Fix styling

--- src/Entities/Account/AccountResponse.php
+++ src/Entities/Account/AccountResponse.php
@@ -2,17 +2,15 @@
 
 namespace Leopaulo88\Asaas\Entities\Account;
 
-use Carbon\Carbon;
 use Leopaulo88\Asaas\Entities\BaseResponse;
+use Leopaulo88\Asaas\Entities\Common\AccountNumber;
+use Leopaulo88\Asaas\Entities\Common\CommercialInfoExpiration;
 
 class AccountResponse extends BaseResponse
 {
-    // Basic info
     public ?string $object;
 
     public ?string $id;
-
-    public ?Carbon $dateCreated;
 
     public ?string $name;
 
@@ -24,7 +22,6 @@ class AccountResponse extends BaseResponse
 
     public ?string $mobilePhone;
 
-    // Address information
     public ?string $address;
 
     public ?string $addressNumber;
@@ -33,15 +30,8 @@ class AccountResponse extends BaseResponse
 
     public ?string $province;
 
-    public ?string $city;
-
-    public ?string $state;
-
-    public ?string $country;
-
     public ?string $postalCode;
 
-    // Document and identification
     public ?string $cpfCnpj;
 
     public ?string $birthDate;
@@ -50,10 +40,21 @@ class AccountResponse extends BaseResponse
 
     public ?string $companyType;
 
-    // Account specific fields
+    public ?string $city;
+
+    public ?string $state;
+
+    public ?string $country;
+
+    public ?string $tradingName;
+
     public ?string $site;
 
-    public ?array $walletId;
+    public ?string $walletId;
+
+    public ?AccountNumber $accountNumber;
+
+    public ?CommercialInfoExpiration $commercialInfoExpiration;
 
     public ?string $apiKey;
 }
---
+++ src/Entities/Common/CommercialInfoExpiration.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Leopaulo88\Asaas\Entities\Common;
+
+use Carbon\Carbon;
+use Leopaulo88\Asaas\Entities\BaseEntity;
+
+class CommercialInfoExpiration extends BaseEntity
+{
+    public function __construct(
+        public ?bool $isExpired = null,
+        public ?Carbon $scheduledDate = null,
+    ) {}
+}
